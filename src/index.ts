2ba1cf79b57347c784c8671e15f36a8b3edd741a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-02-24 15:44:24 -0500

    fix: options argument may be null

--- src/index.ts
+++ src/index.ts
@@ -10,32 +10,37 @@ export interface Options {
   target?: string
 }
 
-export const createTransformer = ({ loaders, ...options }: Options = {}) => ({
-  process(content: string, filename: string) {
-    const ext = getExt(filename)
-    const result = transformSync(content, {
-      loader: loaders?.[ext] || (extname(filename).slice(1) as Loader),
-      format: 'cjs',
-      target: 'es2018',
-      sourcemap: 'external',
-      sourcefile: filename,
-      ...options,
-    })
-    const map = {
-      ...JSON.parse(result.map),
-      sourcesContent: null,
-    }
-    // Append the inline sourcemap manually to ensure the "sourcesContent"
-    // is null. Otherwise, breakpoints won't pause within the actual source.
-    return {
-      code:
-        result.code +
-        '\n//# sourceMappingURL=data:application/json;base64,' +
-        Buffer.from(JSON.stringify(map)).toString('base64'),
-      map,
-    }
-  },
-})
+export const createTransformer = (options: Options | null) => {
+  if (!options) options = {}
+  const { loaders = {} } = options
+  delete options.loaders
+  return {
+    process(content: string, filename: string) {
+      const ext = getExt(filename)
+      const result = transformSync(content, {
+        loader: loaders[ext] || (extname(filename).slice(1) as Loader),
+        format: 'cjs',
+        target: 'es2018',
+        sourcemap: 'external',
+        sourcefile: filename,
+        ...options,
+      })
+      const map = {
+        ...JSON.parse(result.map),
+        sourcesContent: null,
+      }
+      // Append the inline sourcemap manually to ensure the "sourcesContent"
+      // is null. Otherwise, breakpoints won't pause within the actual source.
+      return {
+        code:
+          result.code +
+          '\n//# sourceMappingURL=data:application/json;base64,' +
+          Buffer.from(JSON.stringify(map)).toString('base64'),
+        map,
+      }
+    },
+  }
+}
 
 function getExt(str: string) {
   const basename = path.basename(str)
